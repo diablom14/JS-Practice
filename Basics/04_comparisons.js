e6bba7e30c3c5e132b1e6db5b8d330b663c25887
diablom14 <divyanshuasadeveloper@gmail.com>
2025-05-03 17:52:59 +0530

    Add comparisons file

---
+++ Basics/04_comparisons.js
@@ -0,0 +1,12 @@
+console.log(3 == "3");
+console.log(3 === "3"); // strict equality operator
+
+console.log(null == 0);
+console.log(null > 0); // false
+console.log(null >= 0); // true
+
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined == null); // true
