482069b74bd8d253f7418a8357f0e1d127a50d93
diablom14 <divyanshuasadeveloper@gmail.com>
2025-05-03 17:21:39 +0530

    Add conversion of datatypes

---
+++ Basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score = 33
+
+console.log(typeof score); // number
+console.log(typeof String(score)); // string
+console.log(typeof Boolean(score)); // boolean
+
+let name = "Divyanshu"
+console.log(typeof name); // string
+console.log(typeof Number(name)); // number
+console.log(Number(name)); // NaN
+console.log(typeof Boolean(name)); // boolean
+
+
+
+let isActive = true
+console.log(typeof isActive); // boolean
+
+let str1 = ""
+console.log(typeof Boolean(str1)); // boolean
+console.log(Boolean(str1)); // false
+
+let str2 = "hellow"
+console.log(typeof Boolean(str2)); // boolean
+console.log(Boolean(str2)); // false
