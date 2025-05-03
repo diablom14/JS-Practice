1df4ed477a42784793272fbf3d500610b561c56c
diablom14 <divyanshuasadeveloper@gmail.com>
2025-05-03 17:45:39 +0530

    Add Operations to conversionOperations file

--- Basics/03_conversionOperations.js
+++ Basics/03_conversionOperations.js
@@ -1,24 +1,39 @@
 let score = 33
 
-console.log(typeof score); // number
-console.log(typeof String(score)); // string
-console.log(typeof Boolean(score)); // boolean
+// console.log(typeof score); // number
+// console.log(typeof String(score)); // string
+// console.log(typeof Boolean(score)); // boolean
 
-let name = "Divyanshu"
-console.log(typeof name); // string
-console.log(typeof Number(name)); // number
-console.log(Number(name)); // NaN
-console.log(typeof Boolean(name)); // boolean
+// let name = "Divyanshu"
+// console.log(typeof name); // string
+// console.log(typeof Number(name)); // number
+// console.log(Number(name)); // NaN
+// console.log(typeof Boolean(name)); // boolean
 
 
 
 let isActive = true
-console.log(typeof isActive); // boolean
+// console.log(typeof isActive); // boolean
 
-let str1 = ""
-console.log(typeof Boolean(str1)); // boolean
-console.log(Boolean(str1)); // false
+// let str1 = ""
+// console.log(typeof Boolean(str1)); // boolean
+// console.log(Boolean(str1)); // false
 
-let str2 = "hellow"
-console.log(typeof Boolean(str2)); // boolean
-console.log(Boolean(str2)); // false
+// let str2 = "hellow"
+// console.log(typeof Boolean(str2)); // boolean
+// console.log(Boolean(str2)); // false
+
+// *************** Operations *************** //
+
+console.log("3" + 3); // 33
+console.log("3" - 3); // 0
+console.log("3" * 3); // 9
+console.log("3" / 3); // 1
+console.log("3" % 3); // 0
+
+console.log(1 + 2 + "3"); // 33
+console.log(1 + "2" + 3); // 123
+console.log(3*4+5%3); //Bad practice
+console.log(3*(4+5)%3); //Good practice
+
+
