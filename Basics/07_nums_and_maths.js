af398a60fc331ea0f1d7c5dd8580865c0720716b
diablom14 <divyanshuasadeveloper@gmail.com>
2025-05-04 21:37:27 +0530

    Add nums and maths notes

---
+++ Basics/07_nums_and_maths.js
@@ -0,0 +1,35 @@
+// Numbers
+
+// let num1 = 100
+// console.log(num1.toFixed(2));
+
+// let num2 = new Number(238.8531)
+// console.log(num2.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+// ********************* MATH *************************
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.PI);
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+
+// Random function
+
+console.log(Math.random()); // always returns value between 0 to 1
+console.log(Math.random() * 10); // returns value from 0 to 10
+console.log(Math.floor((Math.random()*10) + 1)); // returns value from 1 to 10
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random()*(max-min+1)) + min));
+
+
+
+
