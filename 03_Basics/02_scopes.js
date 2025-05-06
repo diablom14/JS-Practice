644a3c9282052807a2d149efebb5d489cf350ce4
diablom14 <divyanshuasadeveloper@gmail.com>
2025-05-06 23:00:45 +0530

    Add function scopes and hoisting basics

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -12,4 +12,36 @@ if(true){
 // console.log(b);// gives an error which is right 
 console.log(c); //runs because of global scope creating problems
 
+// Function Scoping example
 
+function one()
+{
+    const username = "Divyanshu"
+
+    function two(){
+        const website = "Youtube"
+        console.log(`${username} is using ${website}`);
+    }
+
+    // console.log(website); // will give error
+    two()
+}
+
+// console.log(username); // will give error
+one()
+
+
+//****** Function hoisting ******
+
+
+
+console.log(addOne(5));// can be called due to function hoisting
+function addOne(a){
+    return a+1
+}
+
+
+// addTwo() // will give error as the function is hold by a const variable
+const addTwo = function(a){
+    return a+2
+}
